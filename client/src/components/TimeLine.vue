ae902cc0fd1f8f967abfc324c6613d94585e04d0
zhaoweixin <627035095@qq.com>
2020-10-19 22:05:17 +0800

    update timeline

--- client/src/components/TimeLine.vue
+++ client/src/components/TimeLine.vue
@@ -28,7 +28,44 @@ export default {
                 srcip_entropy: [],
                 destip_entropy: [],
                 srcport_entropy: [],
-                destport_entropy: []
+                destport_entropy: [],
+            },
+            startTime: null,
+            endTime: null,
+            linelist: ['status', 'priority', 'cpu_load', 'totalbytes', 'srcip_entropy', 'destip_entropy', 'srcport_entropy', 'destport_entropy'],
+            listDict:{
+                'status': {
+                    'Title': 'Status',
+                    'Color': '#747d8c',
+                }, 
+                'priority': {
+                    'Title': 'Priority',
+                    'Color': '#747d8c',
+                },
+                'cpu_load': {
+                    'Title': 'Cpu Load',
+                    'Color': '#747d8c'
+                },
+                'totalbytes': {
+                    'Title': 'Total Bytes',
+                    'Color': '#747d8c'
+                },
+                'srcip_entropy': {
+                    'Title': 'Srcip',
+                    'Color': '#1e90ff'
+                },
+                'destip_entropy': {
+                    'Title': 'Destip',
+                    'Color': '#70a1ff'
+                },
+                'srcport_entropy': {
+                    'Title': 'Srcport',
+                    'Color': '#2ed573'
+                },
+                'destport_entropy': {
+                    'Title': 'Destport',
+                    'Color': '#7bed9f'
+                }
             },
             ScaleYOverview: []
         }
@@ -39,22 +76,16 @@ export default {
         self.DataInit()
     },
     methods:{
-        ChartInit(data){
+        ChartOverviewInit(data){
             //get container width height
             let self = this;
             let ContainerGraphWidth = $('#TimelineGraph').width(),
                 ContainerGraphHeight = $('#TimelineGraph').height(),
-                ContainerOverview = d3.select('#TimelineGraph-Overview'),
-                ContainerDetails = d3.select('#TimelineGraph-Details'),
 
                 ContainerOverviewWidth = $('#TimelineGraph-Overview').width(),
-                ContainerOverviewHeight = $('#TimelineGraph-Overview').height(),
-
-                ContainerDetailsWidth = $('#TimelineGraph-Details').width(),
-                ContainerDetailsHeight = $('#TimelineGraph-Details').height()
+                ContainerOverviewHeight = $('#TimelineGraph-Overview').height()
         
-            let ContainerOverviewMargin = {'top': ContainerOverviewHeight * 0.05, 'bottom': ContainerOverviewHeight * 0.05, 'left': ContainerOverviewWidth * 0.05, 'right': ContainerOverviewWidth * 0.05},
-                ContainerDetailsMargin = {'top': ContainerDetailsHeight * 0.05, 'bottom': ContainerDetailsHeight * 0.05, 'left': ContainerDetailsWidth * 0.05, 'right': ContainerDetailsWidth * 0.05}
+            let ContainerOverviewMargin = {'top': ContainerOverviewHeight * 0.05, 'bottom': ContainerOverviewHeight * 0.05, 'left': ContainerOverviewWidth * 0.05, 'right': ContainerOverviewWidth * 0.05}
             
             //line container space number / line container high
             let ContainerSpace = 10,
@@ -62,62 +93,22 @@ export default {
 
             //svg container border
             let ConOverview = d3.select('#TimelineGraph-Overview').append('svg')
-                .attr('width', ContainerOverviewWidth - ContainerOverviewMargin.right)
+                .attr('width', ContainerOverviewWidth)
                 .attr('height', ContainerOverviewHeight - ContainerOverviewMargin.top - ContainerOverviewMargin.bottom)
                 .attr('transform', () => {return 'translate(0' + ',' + ContainerOverviewMargin.top + ')'})
-                .style('float', 'left'),
-                ConDetails = d3.select('TimelineGraph-Details').append('svg')
-                .attr('width', ContainerDetailsWidth - ContainerDetailsMargin.right)
-                .attr('height', ContainerDetailsHeight - ContainerDetailsMargin.top - ContainerDetailsMargin.bottom)
-                .attr('transform', () => {return 'translate(0' + ',' + ContainerDetailsMargin.top + ')'})
                 .style('float', 'left')
 
             //ScaleX universal
             let ScaleXOverview = d3.scaleTime()
-                .domain(d3.extent(self.timeline_dic.time, (d) => {return d3.timeParse("%Y-%m-%d %H:%M:%S")(d)}))
+                .domain(d3.extent(self.timeline_dic.time, (d,i) => {return d3.timeParse("%Y-%m-%d %H:%M:%S")(d)}))
                 .range([0, ContainerOverviewWidth - ContainerOverviewMargin.left - ContainerOverviewMargin.right]),
                 ScaleXDetails = d3.scaleTime()
 
             // ScaleY customization & line container customization
-            let linelist = ['status', 'priority', 'cpu_load', 'totalbytes', 'srcip_entropy', 'destip_entropy', 'srcport_entropy', 'destport_entropy'],
-                listDict = {
-                    'status': {
-                        'Title': 'Status',
-                        'Color': '#747d8c',
-                    }, 
-                    'priority': {
-                        'Title': 'Priority',
-                        'Color': '#747d8c',
-                    },
-                    'cpu_load': {
-                        'Title': 'Cpu Load',
-                        'Color': '#747d8c'
-                    },
-                    'totalbytes': {
-                        'Title': 'Total Bytes',
-                        'Color': '#747d8c'
-                    },
-                    'srcip_entropy': {
-                        'Title': 'Srcip',
-                        'Color': '#1e90ff'
-                    },
-                    'destip_entropy': {
-                        'Title': 'Destip',
-                        'Color': '#70a1ff'
-                    },
-                    'srcport_entropy': {
-                        'Title': 'Srcport',
-                        'Color': '#2ed573'
-                    },
-                    'destport_entropy': {
-                        'Title': 'Destport',
-                        'Color': '#7bed9f'
-                    }
-                },
-                ScaleY = {},
+            let ScaleY = {},
                 ContainerLineEntity = {}
-
-            linelist.forEach((d,i) => {
+            
+            self.linelist.forEach((d,i) => {
                 ScaleY[d] = {}
                 ScaleY[d]['ScaleY'] = {}
                 ScaleY[d]['ScaleY']['name'] = d
@@ -157,48 +148,195 @@ export default {
                 // line generator
                 let line = d3.line()
                     .x((v) => {return ScaleXOverview(d3.timeParse("%Y-%m-%d %H:%M:%S")(v.x))})
-                    .y((v) => {return ScaleY[linelist[order]]['ScaleY']['entity'](v.y)})
+                    .y((v) => {return ScaleY[self.linelist[order]]['ScaleY']['entity'](v.y)})
                     .curve(d3.curveMonotoneX)
                 // line container position
-                ContainerLineEntity[linelist[order]]['container'].attr('transform', () => {return "translate(" + ContainerOverviewMargin.left +"," +  (i * ContainerItemsHeight ) + ")"})
+                ContainerLineEntity[self.linelist[order]]['container'].attr('transform', () => {return "translate(" + ContainerOverviewMargin.left +"," +  (i * ContainerItemsHeight ) + ")"})
                 //draw AxisX
-                ContainerLineEntity[linelist[order]]['axis'] = ContainerLineEntity[linelist[order]]['container']
+                ContainerLineEntity[self.linelist[order]]['axis'] = ContainerLineEntity[self.linelist[order]]['container']
                     .append('g').attr('transform', () => {return "translate(0" +"," + ContainerItemsHeight + ')'}).call(AxisXtemp)
                 //draw line
-                ContainerLineEntity[linelist[order]]['line'] = ContainerLineEntity[linelist[order]]['container']
+                ContainerLineEntity[self.linelist[order]]['line'] = ContainerLineEntity[self.linelist[order]]['container']
                     .append('path')
                     .datum(() => {
                         let data = [],
-                            key = linelist[order]
+                            key = self.linelist[order]
                         self.timeline_datalist.forEach((d) => {
                             data.push({'x': d['time'], 'y': d[key]})
                         })
                         return data
                     })
-                    .attr('type', linelist[order])
+                    .attr('type', self.linelist[order])
                     .attr('fill', 'none')
                     .attr('stroke', () => {
-                        return listDict[linelist[order]]['Color']
+                        return self.listDict[self.linelist[order]]['Color']
                     })
                     .attr('stroke-width', 1.5)
                     .attr("d", line)
-                    .attr('class', () => {return 'ov-line-' + linelist[order]})
+                    .attr('class', () => {return 'ov-line-' + self.linelist[order]})
 
-                 ContainerLineEntity[linelist[order]]['text'] = ContainerLineEntity[linelist[order]]['container']
+                 ContainerLineEntity[self.linelist[order]]['text'] = ContainerLineEntity[self.linelist[order]]['container']
                  .append('text')
                  .text(() => {
-                     return listDict[linelist[order]]['Title']
+                     return self.listDict[self.linelist[order]]['Title']
                  })
                  .style('fill', '#515a6e')
+                 .style('font-size', 11)
                  .attr('transform', () => {return "translate(" + (-(ContainerOverviewMargin.left / 1.5)) + ',' + (ContainerItemsHeight) + ')'})
             }
 
             //not display axis line
             d3.select('#TimelineGraph-Overview').selectAll('.domain').style('opacity', 0)
 
+            //Add text
+            let ConOverviewTextOverview = ConOverview.append('g').attr('id', 'ConOverviewTextOverview')
+                .attr('transform', 'translate(' + ( ContainerOverviewWidth - ContainerOverviewMargin.left + (ContainerOverviewMargin.right / 3) )  + ',' + (ContainerOverviewHeight - ContainerOverviewMargin.top - ContainerOverviewMargin.bottom) / 5 * 1.5 + ')')
+                .append('text').text('Overview')
+                .style('font-size', 12)
+                .style('fill', '#515a6e')
+                .style('font-weight', 900)
+
+            let ConOverviewTextEntropy = ConOverview.append('g').attr('id', 'ConOverviewTextEntropy')
+                .attr('transform', 'translate(' + ( ContainerOverviewWidth - ContainerOverviewMargin.left + (ContainerOverviewMargin.right / 3) )  + ',' + (ContainerOverviewHeight - ContainerOverviewMargin.top - ContainerOverviewMargin.bottom) / 5 * 4 + ')')
+                .append('text').text('Entropy')
+                .style('font-size', 12)
+                .style('fill', '#515a6e')
+                .style('font-weight', 900)
+
+            
+            //Add beauty bar
+            let ConOverviewBars = ConOverview.append('g').attr('id', 'ConOverviewBars')
+                .attr('transform', () => {
+                    return 'translate(' + (ContainerOverviewWidth - ContainerOverviewMargin.left + (ContainerOverviewMargin.right / 5)) + ',' +  ContainerOverviewMargin.top + ')'
+                })
+                .append('rect')
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', 5)
+                .attr('height', () => {return ContainerOverviewHeight - ContainerOverviewMargin.top - ContainerOverviewMargin.bottom})
+                .style('fill', 'grey')
+
+            let ConOverviewCoverBars = ConOverview.append('g').attr('id', 'ConOverviewCoverBars')
+                .attr('transform', () => {
+                    return 'translate(' + (ContainerOverviewWidth - ContainerOverviewMargin.left + (ContainerOverviewMargin.right / 5)) + ',' + (ContainerOverviewMargin.top + ContainerItemsHeight * gap) + ')'})
+                .append('rect')
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', 5)
+                .attr('height', () => {return ContainerItemsHeight})
+                .style('fill', 'white')
+
+            
+
+            this.ChartDetailInit({start: "2013-04-15 01:00:00", end: "2013-04-15 02:00:00"})
+            
 
 
         //draw bottom axis
+        },
+        ChartDetailInit(TimePeriod){
+            // data start time: "2013-04-10 07:05:00" 
+            // data end time: "2013-04-15 10:00:00"
+            // timesegment {'start': 2013-04-15 01:00:00, 'end': 2013-04-15 02:00:00}
+            let self = this;
+
+            //get container width height margin
+            let ContainerOverview = d3.select('#TimelineGraph-Overview'),
+                ContainerDetails = d3.select('#TimelineGraph-Details'),
+                ContainerDetailsWidth = $('#TimelineGraph-Details').width(),
+                ContainerDetailsHeight = $('#TimelineGraph-Details').height()
+
+            let ContainerDetailsMargin = {'top': ContainerDetailsHeight * 0.05, 'bottom': ContainerDetailsHeight * 0.25, 'left': ContainerDetailsWidth * 0.05, 'right': ContainerDetailsWidth * 0.05}
+
+            //svg container border
+            let ConDetails = d3.select('#TimelineGraph-Details').append('svg')
+                .attr('width', ContainerDetailsWidth)
+                .attr('height', ContainerDetailsHeight - ContainerDetailsMargin.top)
+                .attr('transform', () => {return 'translate(0' + ',' + ContainerDetailsMargin.top + ')'})
+                .style('float', 'left')
+
+            //CALCULATION
+            //startIndex endIndex store source time index
+            let startIndex = Math.abs(new Date(TimePeriod.start) - new Date(self.startTime)) / 300000,
+                endIndex = Math.abs(new Date(TimePeriod.end) - new Date(self.startTime)) / 300000,
+                linedata = []
+            
+            for(let i=startIndex; i<=endIndex; i++){
+                linedata.push(self.timeline_datalist[i])
+            }
+
+            //ScaleX universal
+            let ScaleXDetails = d3.scaleTime()
+                .domain(d3.extent(self.timeline_dic.time, (d,i) => {if(startIndex <= i && i <= endIndex){return d3.timeParse("%Y-%m-%d %H:%M:%S")(d)}}))
+                .range([0, ContainerDetailsWidth - ContainerDetailsMargin.left - ContainerDetailsMargin.right])
+
+            // ScaleY customization & line container customization
+            let ScaleYDetails = null,
+                linelist = ['srcip_entropy', 'destip_entropy', 'srcport_entropy', 'destport_entropy'],
+                linelistmax = 0,
+                linelistmin = 0
+            
+            linelist.forEach((d,i) => {
+                let max = Math.max.apply(Math, self.timeline_datalist.map(function(o) {return o[d]})),
+                    min = Math.min.apply(Math, self.timeline_datalist.map(function(o) {return o[d]}))
+                if(max > linelistmax){
+                    linelistmax = max
+                }
+                if(min < linelistmin){
+                    linelistmin = min
+                }
+            })
+
+            ScaleYDetails = d3.scaleLinear().domain([linelistmin, linelistmax]).range([ContainerDetailsHeight - ContainerDetailsMargin.top - ContainerDetailsMargin.bottom, 0])
+
+            //Add line
+            let ContainerLineEntity = {},
+                ConDetailsLines = ConDetails.append('g')
+                    .attr('id', 'ConDetailsLines')
+                    .attr('transform', 'translate(' + ContainerDetailsMargin.left + ',0)'),
+                Linegenerator = d3.line()
+                    .x((v) => {return ScaleXDetails(d3.timeParse("%Y-%m-%d %H:%M:%S")(v.x))})
+                    .y((v) => {return ScaleYDetails(v.y)})
+                    .curve(d3.curveMonotoneX)
+
+            linelist.forEach((d,i) => {
+                let drawdata = []
+                linedata.forEach((v,i) => {
+                    drawdata.push({'x': v['time'], 'y': v[d]})
+                })
+                ContainerLineEntity[d] = {}
+                ContainerLineEntity[d]['line'] = ConDetailsLines.append('path')
+                    .datum(drawdata)
+                    .attr('d', Linegenerator)
+                    .attr('fill', 'none')
+                    .attr('stroke-width', 1.5)
+                    .attr('stroke', self.listDict[d]['Color'])
+            })
+            //Add Axis
+            let ConDetailsAxis = ConDetails.append('g').attr('id', 'ConDetailsAxis')
+                .attr('transform', () => {return "translate(" + ContainerDetailsMargin.left +"," + (ContainerDetailsHeight - ContainerDetailsMargin.top - ContainerDetailsMargin.bottom) + ')'})
+                .call(d3.axisBottom(ScaleXDetails))
+
+            //Add text
+            let ConDetailsText = ConDetails.append('g').attr('id', 'ConDetailsText')
+                .attr('transform', 'translate(' + ( ContainerDetailsWidth - ContainerDetailsMargin.left + (ContainerDetailsMargin.right / 3) )  + ',' + ContainerDetailsHeight / 2 + ')')
+                .append('text').text('Details')
+                .style('font-size', 12)
+                .style('fill', '#515a6e')
+                .style('font-weight', 900)
+            //Add beauty bar
+            let ConDetailsBars = ConDetails.append('g')
+                .attr('transform', 'translate(' + ( ContainerDetailsWidth - ContainerDetailsMargin.left + (ContainerDetailsMargin.right / 5) )  + ',' + ContainerDetailsMargin.top + ')')
+                .append('rect')
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', 5)
+                .attr('height', () => {return ContainerDetailsHeight - ContainerDetailsMargin.top - ContainerDetailsMargin.bottom})
+                .style('fill', 'grey')
+                
+            
+
+            
         },
         DataInit(){
             let self = this;
@@ -221,9 +359,10 @@ export default {
                 })
                 return obj
             }).then((data) => {
-
+                self.startTime = data[0].time;
+                self.endTime = data[data.length - 1].time;
                 self.timeline_datalist = data;
-                self.ChartInit(data)
+                self.ChartOverviewInit(data)
             })
         },
         fnResize(){
@@ -241,25 +380,25 @@ export default {
 
 #TimelineTitle{
     width: 100%;
-    height:15%;
+    height: 5%;
     float: left;
 }
 
 #TimelineGraph {
     width: 100%;
-    height: 85%;
+    height: 95%;
     float: left;
 }
 
 #TimelineGraph-Overview {
     width: 100%;
-    height: 80%;
+    height: 70%;
     float: left;
 }
 
 #TimelineGraph-Details {
     width: 100%;
-    height: 20%;
+    height: 30%;
     float: left;
 }
 
